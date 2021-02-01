fe1e2f7fc76336c3505f4de93e7a98abe8da757f
mattcln <matthieu.cln@gmail.com>
2021-02-01 14:59:53 +0100

    Link in a new window

--- client/portfolio.js
+++ client/portfolio.js
@@ -61,7 +61,7 @@ const renderProducts = products => {
       return `
       <div class="product" id=${product.uuid}>
         <span>${product.brand}</span>
-        <a href="${product.link}">${product.name}</a>
+        <a href="${product.link}" target="_blank">${product.name}</a>
         <span>${product.price}</span>
       </div>
     `;
